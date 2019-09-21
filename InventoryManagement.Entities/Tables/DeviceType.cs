bf4a591d199dc002e3f56b65b9698ce28b74342a
Fatih <fatih47154@gmail.com>
2019-09-21 17:38:35 +0300

    Project Files

---
+++ InventoryManagement.Entities/Tables/DeviceType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryManagement.Entities.Abstract;
+
+namespace InventoryManagement.Entities.Tables
+{
+    public class DeviceType : ABaseEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public char Character { get; set; }
+    }
+}
---
+++ InventoryManagement.Entities/Tables/Locations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryManagement.Entities.Abstract;
+
+namespace InventoryManagement.Entities.Tables
+{
+    public class Locations : ABaseEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
